0e56db4f2a1274fb3d2bcbfa52f4e233bf0b4a57
jeremystone <jeremymichaelstone@gmail.com>
2019-03-20 13:21:40 +0000

    MTDSA-3148 - Models for intent to crystallise

---
+++ app/v2/models/des/DesCalculationIdResponse.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package v2.models.des
+
+import play.api.libs.json.{Json, Reads}
+
+/**
+  * Calculation Id returned from DES as part of intent-to-crystallise
+  */
+case class DesCalculationIdResponse(id: String)
+
+object DesCalculationIdResponse {
+  implicit val reads: Reads[DesCalculationIdResponse] = Json.reads
+}
